0c42d09f762efb509db853d1999d2e806a65e4da
Danang Rahmatullah <dasyad00@gmail.com>
2022-05-24 23:23:13 +0200

    get target port and address from launch file

--- rosbridge_client/scripts/rosbridge_websocket.py
+++ rosbridge_client/scripts/rosbridge_websocket.py
@@ -6,8 +6,8 @@ from autobahn.twisted.websocket import WebSocketClientFactory, connectWS
 from twisted.internet import reactor
 from twisted.internet.error import ReactorNotRunning
 
-
 from rosbridge_client.autobahn_websocket import RosbridgeWebSocket
+
 
 def shutdown_hook():
     try:
@@ -24,6 +24,26 @@ if __name__ == "__main__":
     ##################################################
     retry_startup_delay = rospy.get_param('~retry_startup_delay', 2.0)  # seconds
 
+    # target port and address should be defined in the launch file!
+    port = rospy.get_param('~port')
+    address = rospy.get_param('~address')
+
+    if "--port" in sys.argv:
+        idx = sys.argv.index("--port") + 1
+        if idx < len(sys.argv):
+            port = int(sys.argv[idx])
+        else:
+            print("--port argument provided without a value.")
+            sys.exit(-1)
+
+    if "--address" in sys.argv:
+        idx = sys.argv.index("--address") + 1
+        if idx < len(sys.argv):
+            address = str(sys.argv[idx])
+        else:
+            print("--address argument provided without a value.")
+            sys.exit(-1)
+
     if "--retry_startup_delay" in sys.argv:
         idx = sys.argv.index("--retry_startup_delay") + 1
         if idx < len(sys.argv):
@@ -32,10 +52,45 @@ if __name__ == "__main__":
             print("--retry_startup_delay argument provided without a value.")
             sys.exit(-1)
 
-    # TODO get parameters from launch file
-    address = "ws://localhost"
-    port = 9090
-    uri = "{}:{}".format(address, port)
+    if "--topics_glob" in sys.argv:
+        idx = sys.argv.index("--topics_glob") + 1
+        if idx < len(sys.argv):
+            value = sys.argv[idx]
+            if value == "None":
+                RosbridgeWebSocket.topics_glob = []
+            else:
+                RosbridgeWebSocket.topics_glob = [element.strip().strip("'") for element in value[1:-1].split(',')]
+        else:
+            print("--topics_glob argument provided without a value. (can be None or a list)")
+            sys.exit(-1)
+
+    if "--services_glob" in sys.argv:
+        idx = sys.argv.index("--services_glob") + 1
+        if idx < len(sys.argv):
+            value = sys.argv[idx]
+            if value == "None":
+                RosbridgeWebSocket.services_glob = []
+            else:
+                RosbridgeWebSocket.services_glob = [element.strip().strip("'") for element in value[1:-1].split(',')]
+        else:
+            print("--services_glob argument provided without a value. (can be None or a list)")
+            sys.exit(-1)
+
+    if "--params_glob" in sys.argv:
+        idx = sys.argv.index("--params_glob") + 1
+        if idx < len(sys.argv):
+            value = sys.argv[idx]
+            if value == "None":
+                RosbridgeWebSocket.params_glob = []
+            else:
+                RosbridgeWebSocket.params_glob = [element.strip().strip("'") for element in value[1:-1].split(',')]
+        else:
+            print("--params_glob argument provided without a value. (can be None or a list)")
+            sys.exit(-1)
+
+    # TODO support wss
+    protocol = "ws"
+    uri = "{}://{}:{}".format(protocol, address, port)
     factory = WebSocketClientFactory(uri)
     factory.protocol = RosbridgeWebSocket
 
