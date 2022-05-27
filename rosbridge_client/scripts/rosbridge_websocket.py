4d9a5765b10adee08143d5257ae4d6b06fada2c2
Danang Rahmatullah <dasyad00@gmail.com>
2022-05-27 22:28:10 +0200

    copy remaining relevant paramaters

--- rosbridge_client/scripts/rosbridge_websocket.py
+++ rosbridge_client/scripts/rosbridge_websocket.py
@@ -3,10 +3,18 @@ import sys
 import rospy
 
 from autobahn.twisted.websocket import WebSocketClientFactory, connectWS
-from twisted.internet import reactor
+from twisted.internet import reactor, ssl
 from twisted.internet.error import ReactorNotRunning
+from autobahn.websocket.compress import (PerMessageDeflateOffer, PerMessageDeflateOfferAccept)
 
 from rosbridge_client.autobahn_websocket import RosbridgeWebSocket
+
+from rosbridge_library.capabilities.advertise import Advertise
+from rosbridge_library.capabilities.publish import Publish
+from rosbridge_library.capabilities.subscribe import Subscribe
+from rosbridge_library.capabilities.advertise_service import AdvertiseService
+from rosbridge_library.capabilities.unadvertise_service import UnadvertiseService
+from rosbridge_library.capabilities.call_service import CallService
 
 
 def shutdown_hook():
@@ -24,10 +32,53 @@ if __name__ == "__main__":
     ##################################################
     retry_startup_delay = rospy.get_param('~retry_startup_delay', 2.0)  # seconds
 
+    use_compression = rospy.get_param('~use_compression', False)
+
+    # RosbridgeProtocol parameters
+    RosbridgeWebSocket.fragment_timeout = rospy.get_param(
+        '~fragment_timeout', RosbridgeWebSocket.fragment_timeout
+    )
+    RosbridgeWebSocket.delay_between_messages = rospy.get_param(
+        '~delay_between_messages', RosbridgeWebSocket.delay_between_messages
+    )
+    RosbridgeWebSocket.max_message_size = rospy.get_param(
+        '~max_message_size', RosbridgeWebSocket.max_message_size
+    )
+    RosbridgeWebSocket.unregister_timeout = rospy.get_param(
+        '~unregister_timeout', RosbridgeWebSocket.unregister_timeout
+    )
+    bson_only_mode = rospy.get_param('~bson_only_mode', False)
+
+    if RosbridgeWebSocket.max_message_size == "None":
+        RosbridgeWebSocket.max_message_size = None
+
+    ping_interval = float(rospy.get_param('~websocket_ping_interval', 0))
+    ping_timeout = float(rospy.get_param('~websocket_ping_timeout', 30))
+
+    # SSL options
+    certfile = rospy.get_param('~certfile', None)
+    keyfile = rospy.get_param('~keyfile', None)
+    # if authentication should be used
+    RosbridgeWebSocket.authenticate = rospy.get_param('~authenticate', False)
+
     # target port and address should be defined in the launch file!
     port = rospy.get_param('~port')
     address = rospy.get_param('~address')
     endpoint = rospy.get_param('~endpoint', "/")
+
+    # Get the glob strings and parse them as arrays.
+    RosbridgeWebSocket.topics_glob = [
+        element.strip().strip("'")
+        for element in rospy.get_param('~topics_glob', '')[1:-1].split(',')
+        if len(element.strip().strip("'")) > 0]
+    RosbridgeWebSocket.services_glob = [
+        element.strip().strip("'")
+        for element in rospy.get_param('~services_glob', '')[1:-1].split(',')
+        if len(element.strip().strip("'")) > 0]
+    RosbridgeWebSocket.params_glob = [
+        element.strip().strip("'")
+        for element in rospy.get_param('~params_glob', '')[1:-1].split(',')
+        if len(element.strip().strip("'")) > 0]
 
     if "--port" in sys.argv:
         idx = sys.argv.index("--port") + 1
@@ -51,6 +102,42 @@ if __name__ == "__main__":
             retry_startup_delay = int(sys.argv[idx])
         else:
             print("--retry_startup_delay argument provided without a value.")
+            sys.exit(-1)
+
+    if "--fragment_timeout" in sys.argv:
+        idx = sys.argv.index("--fragment_timeout") + 1
+        if idx < len(sys.argv):
+            RosbridgeWebSocket.fragment_timeout = int(sys.argv[idx])
+        else:
+            print("--fragment_timeout argument provided without a value.")
+            sys.exit(-1)
+
+    if "--delay_between_messages" in sys.argv:
+        idx = sys.argv.index("--delay_between_messages") + 1
+        if idx < len(sys.argv):
+            RosbridgeWebSocket.delay_between_messages = float(sys.argv[idx])
+        else:
+            print("--delay_between_messages argument provided without a value.")
+            sys.exit(-1)
+
+    if "--max_message_size" in sys.argv:
+        idx = sys.argv.index("--max_message_size") + 1
+        if idx < len(sys.argv):
+            value = sys.argv[idx]
+            if value == "None":
+                RosbridgeWebSocket.max_message_size = None
+            else:
+                RosbridgeWebSocket.max_message_size = int(value)
+        else:
+            print("--max_message_size argument provided without a value. (can be None or <Integer>)")
+            sys.exit(-1)
+
+    if "--unregister_timeout" in sys.argv:
+        idx = sys.argv.index("--unregister_timeout") + 1
+        if idx < len(sys.argv):
+            unregister_timeout = float(sys.argv[idx])
+        else:
+            print("--unregister_timeout argument provided without a value.")
             sys.exit(-1)
 
     if "--topics_glob" in sys.argv:
@@ -89,11 +176,68 @@ if __name__ == "__main__":
             print("--params_glob argument provided without a value. (can be None or a list)")
             sys.exit(-1)
 
-    # TODO support wss
-    protocol = "ws"
+    if ("--bson_only_mode" in sys.argv) or bson_only_mode:
+        RosbridgeWebSocket.bson_only_mode = bson_only_mode
+
+    if "--websocket_ping_interval" in sys.argv:
+        idx = sys.argv.index("--websocket_ping_interval") + 1
+        if idx < len(sys.argv):
+            ping_interval = float(sys.argv[idx])
+        else:
+            print("--websocket_ping_interval argument provided without a value.")
+            sys.exit(-1)
+
+    if "--websocket_ping_timeout" in sys.argv:
+        idx = sys.argv.index("--websocket_ping_timeout") + 1
+        if idx < len(sys.argv):
+            ping_timeout = float(sys.argv[idx])
+        else:
+            print("--websocket_ping_timeout argument provided without a value.")
+            sys.exit(-1)
+
+    # To be able to access the list of topics and services, you must be able to access the rosapi services.
+    if RosbridgeWebSocket.services_glob:
+        RosbridgeWebSocket.services_glob.append("/rosapi/*")
+
+    Subscribe.topics_glob = RosbridgeWebSocket.topics_glob
+    Advertise.topics_glob = RosbridgeWebSocket.topics_glob
+    Publish.topics_glob = RosbridgeWebSocket.topics_glob
+    AdvertiseService.services_glob = RosbridgeWebSocket.services_glob
+    UnadvertiseService.services_glob = RosbridgeWebSocket.services_glob
+    CallService.services_glob = RosbridgeWebSocket.services_glob
+
+    # Support the legacy "" address value.
+    # The socket library would interpret this as INADDR_ANY.
+    if not address:
+        address = '0.0.0.0'
+
+    ##################################################
+    # Done with parameter handling                   #
+    ##################################################
+
+    def handle_compression_offers(offers):
+        if not use_compression:
+            return
+        for offer in offers:
+            if isinstance(offer, PerMessageDeflateOffer):
+                return PerMessageDeflateOfferAccept(offer)
+
+    if certfile is not None and keyfile is not None:
+        protocol = 'wss'
+        context_factory = ssl.DefaultOpenSSLContextFactory(keyfile, certfile)
+    else:
+        protocol = 'ws'
+        context_factory = None
+
+    # TODO testing port
     uri = "{}://{}:{}{}".format(protocol, address, port, endpoint)
     factory = WebSocketClientFactory(uri)
     factory.protocol = RosbridgeWebSocket
+    factory.setProtocolOptions(
+        perMessageCompressionAccept=handle_compression_offers,
+        autoPingInterval=ping_interval,
+        autoPingTimeout=ping_timeout,
+    )
 
     connected = False
     while not connected and not rospy.is_shutdown():
--- rosbridge_client/src/rosbridge_client/autobahn_websocket.py
+++ rosbridge_client/src/rosbridge_client/autobahn_websocket.py
@@ -1,12 +1,226 @@
+import rospy
+
+from rosauth.srv import Authentication
+
+import sys
+import threading
+import traceback
+from functools import wraps
+from collections import deque
+
 from autobahn.twisted.websocket import WebSocketClientProtocol
+from twisted.internet import interfaces, reactor
+from zope.interface import implementer
+
+from rosbridge_library.rosbridge_protocol import RosbridgeProtocol
+from rosbridge_library.util import json, bson
+
+
+# directly copied from rosbridge_server/autobahn_websocket.py
+def _log_exception():
+    """Log the most recent exception to ROS."""
+    exc = traceback.format_exception(*sys.exc_info())
+    rospy.logerr(''.join(exc))
+
+
+def log_exceptions(f):
+    """Decorator for logging exceptions to ROS."""
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except:
+            _log_exception()
+            raise
+
+    return wrapper
+
+
+class IncomingQueue(threading.Thread):
+    """Decouples incoming messages from the Autobahn thread.
+
+    This mitigates cases where outgoing messages are blocked by incoming,
+    and vice versa.
+    """
+
+    def __init__(self, protocol):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.queue = deque()
+        self.protocol = protocol
+
+        self.cond = threading.Condition()
+        self._finished = False
+
+    def finish(self):
+        """Clear the queue and do not accept further messages."""
+        with self.cond:
+            self._finished = True
+            while len(self.queue) > 0:
+                self.queue.popleft()
+            self.cond.notify()
+
+    def push(self, msg):
+        with self.cond:
+            self.queue.append(msg)
+            self.cond.notify()
+
+    def run(self):
+        while True:
+            with self.cond:
+                if len(self.queue) == 0 and not self._finished:
+                    self.cond.wait()
+
+                if self._finished:
+                    break
+
+                msg = self.queue.popleft()
+
+            self.protocol.incoming(msg)
+
+        self.protocol.finish()
+
+
+@implementer(interfaces.IPushProducer)
+class OutgoingValve:
+    """Allows the Autobahn transport to pause outgoing messages from rosbridge.
+
+    The purpose of this valve is to connect backpressure from the WebSocket client
+    back to the rosbridge protocol, which depends on backpressure for queueing.
+    Without this flow control, rosbridge will happily keep writing messages to
+    the WebSocket until the system runs out of memory.
+
+    This valve is closed and opened automatically by the Twisted TCP server.
+    In practice, Twisted should only close the valve when its userspace write buffer
+    is full and it should only open the valve when that buffer is empty.
+
+    When the valve is closed, the rosbridge protocol instance's outgoing writes
+    must block until the valve is opened.
+    """
+
+    def __init__(self, proto):
+        self._proto = proto
+        self._valve = threading.Event()
+        self._finished = False
+
+    @log_exceptions
+    def relay(self, message):
+        self._valve.wait()
+        if self._finished:
+            return
+        reactor.callFromThread(self._proto.outgoing, message)
+
+    def pauseProducing(self):
+        if not self._finished:
+            self._valve.clear()
+
+    def resumeProducing(self):
+        self._valve.set()
+
+    def stopProducing(self):
+        self._finished = True
+        self._valve.set()
+
+
+# end of direct copy from rosbridge_server/autobahn_websocket.py
 
 
 class RosbridgeWebSocket(WebSocketClientProtocol):
+    authenticate = False
+
+    # The following are passed on to RosbridgeProtocol
+    # defragmentation.py:
+    fragment_timeout = 600  # seconds
+    # protocol.py:
+    delay_between_messages = 0  # seconds
+    max_message_size = None  # bytes
+    unregister_timeout = 10.0  # seconds
+    bson_only_mode = False
+
     def onOpen(self):
-        pass
+        cls = self.__class__
+        try:
+            self.protocol = RosbridgeProtocol(
+                client_id=0,
+                parameters=self.get_parameters()
+            )
+            self.incoming_queue = IncomingQueue(self.protocol)
+            self.incoming_queue.start()
+            producer = OutgoingValve(self)
+            self.transport.outgoing = producer.relay
+            self.authenticated = False
+            self.target = self.transport.getPeer().host
+            rospy.loginfo("Client connected to {}".format(self.target))
+        except Exception as exc:
+            rospy.logerr("Unable to accept incoming connection.  Reason: %s", str(exc))
+        if cls.authenticate:
+            rospy.loginfo("Awaiting proper authentication...")
 
-    def onMessage(self, payload, isBinary):
-        pass
+    def onMessage(self, message, binary):
+        cls = self.__class__
+        if not binary:
+            message = message.decode('utf-8')
+        # check if we need to authenticate
+        if cls.authenticate and not self.authenticated:
+            try:
+                if cls.bson_only_mode:
+                    msg = bson.BSON(message).decode()
+                else:
+                    msg = json.loads(message)
 
-    def onClose(self, wasClean, code, reason):
-        pass
+                if msg['op'] == 'auth':
+                    # check the authorization information
+                    auth_srv = rospy.ServiceProxy('authenticate', Authentication)
+                    resp = auth_srv(
+                        msg['mac'], msg['client'], msg['dest'],
+                        msg['rand'], rospy.Time(msg['t']),
+                        msg['level'], rospy.Time(msg['end'])
+                    )
+                    self.authenticated = resp.authenticated
+                    if self.authenticated:
+                        rospy.loginfo("Client has authenticated.")
+                        return
+
+                # if we are here, no valid authentication was given
+                rospy.logwarn("Client %d did not authenticate. Closing connection.",
+                              self.protocol.client_id)
+                self.sendClose()
+            except:
+                # proper error will be handled in the protocol class
+                self.incoming_queue.push(message)
+        else:
+            # no authentication required
+            self.incoming_queue.push(message)
+
+    def outgoing(self, message):
+        if type(message) == bson.BSON:
+            binary = True
+            message = bytes(message)
+        elif type(message) == bytearray:
+            binary = True
+            message = bytes(message)
+        else:
+            binary = False
+            message = message.encode('utf-8')
+
+        self.sendMessage(message, binary)
+
+    def onClose(self, was_clean, code, reason):
+        rospy.loginfo("Client disconnected. {}. code={}".format(reason, code))
+        if not hasattr(self, 'protocol'):
+            return  # Closed before connection was opened.
+
+        self.incoming_queue.finish()
+        # TODO
+        # rospy.signal_shutdown(reason)
+
+    @classmethod
+    def get_parameters(cls):
+        return {
+            "fragment_timeout": cls.fragment_timeout,
+            "delay_between_messages": cls.delay_between_messages,
+            "max_message_size": cls.max_message_size,
+            "unregister_timeout": cls.unregister_timeout,
+            "bson_only_mode": cls.bson_only_mode
+        }
